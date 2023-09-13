97a38ce67b36725d6ff48bb6253eb00654b63181
Bruno Pena-Lepe <bpena-lepe@cps.edu>
2023-09-13 11:42:37 -0500

    duckduckgoose

--- a1.py
+++ a1.py
@@ -147,16 +147,21 @@ def duck_duck_goose(lst: List[str]) -> List[str]:
     Returns:
         the resulting list after playing duck duck goose
     """
-    count=1
+    count=0
+    current=1
     while len(lst)>2:
-        if count%3==0:
-            lst.pop(count-1)
-            if(count>=len(lst)):
-                count=(count%3)+1
+        if current==1:
+            current=2
+            count+=1
+        elif current==2:
+            current=3
+            count+=1
+        else:
+            lst.pop(count)
+            current=1
+        if count>=len(lst):
+            count-=len(lst)
             
-            #count-=1
-            
-        count+=1
     return lst
     raise NotImplementedError("duck_duck_goose")
 
